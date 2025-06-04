dde603c8f3d6c766b2768046fcb97c8f2500df71
Peter Nilsson <peter8nilsson@live.se>
2025-06-04 21:16:48 +0200

    [setup] Improved link generation
    Link generation should hopefully be a *bit* more intelligent
    now, and should work with linking directories.
    
    No longer override .bak file with the link if you run the script
    multiple times.
    
    We now skip linking if the destination link already exists.

--- setup.9-generate-links.sh
+++ setup.9-generate-links.sh
@@ -2,18 +2,42 @@
 
 set -eo pipefail
 
+declare -i new_links=0
+declare -i backups=0
+declare -i skipped=0
+
 GenerateLink() {
-    if [ -f $2 ]
-    then
-        #echo "Backing up $2 to $2.bak"
-        cp $2 $2.bak
+    if [ -e $2 ]; then
+
+        # If target already exists we make a backup
+        if [ ! -e $2.bak ]; then
+            echo "Backing up $2 to $2.bak"
+            cp -r $2 $2.bak
+
+            backups+=1
+
+            if [ $? -eq 0 ] && [ -d $2 ]; then
+                # If we succeeded in making a backup and the original
+                # is a directory we need to delete the original folder.
+                # Otherwise the symbolic link will be placed *inside* that folder.
+
+                rm -rf $2 # NOTE: I'm not the biggest fan of doing this tbh...
+            fi
+        fi
+
     else
-        #echo "Creating directory $(dirname "$2")"
+        echo "Creating directory $(dirname "$2")"
         mkdir -p "$(dirname "$2")"
     fi
 
-    echo "$2 -> $1"
-    ln -sf $1 $2
+    if [ -L $2 ]; then
+        skipped+=1
+        echo "$2 already linked. Skipping..."
+    else
+        echo "$2 -> $1"
+        ln -sf $1 $2
+        new_links+=1
+    fi
 }
 
 usrcfg=$HOME/.config
@@ -37,4 +61,5 @@ GenerateLink $(pwd)/config/zed/settings.json $usrcfg/zed/settings.json
 GenerateLink $(pwd)/config/kitty/kitty.conf $usrcfg/kitty/kitty.conf
 GenerateLink $(pwd)/config/kitty/tokyo-night.conf $usrcfg/kitty/tokyo-night.conf
 
-echo "Done"
+echo ""
+echo "Done. $new_links new links. $skipped skipped. $backups backups."
