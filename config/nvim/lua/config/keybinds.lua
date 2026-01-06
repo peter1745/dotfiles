6f4c30ce6fe2b3fa809a7757f4dabff158564bc5
Peter Nilsson <peter8nilsson@live.se>
2026-01-06 13:05:33 +0100

    [nvim] Add Telescope

--- config/nvim/lua/config/keybinds.lua
+++ config/nvim/lua/config/keybinds.lua
@@ -8,6 +8,25 @@ map("n", "<C-q>", "<cmd>tabclose<CR>", { desc = "Close Current Tab" })
 map("n", "<C-A-Right>", "<cmd>tabnext<CR>", { desc = "Next Tab" })
 map("n", "<C-A-Left>", "<cmd>tabprevious<CR>", { desc = "Previous Tab" })
 
+-- Telescope Bindings
+local telescope = require("telescope.builtin")
+map("n", "<C-o>", telescope.find_files, { desc = "Find Files" })
+map("n", "<C-m>", telescope.man_pages, { desc = "Find Files" })
+map("n", "<A-m>", telescope.lsp_document_symbols, { desc = "List Symbols" })
+
+local use_live_grep = false
+
+if use_live_grep then
+    map("n", "<C-g>", telescope.live_grep, { desc = "Find in Files" })
+else
+    map("n", "<C-g>", function()
+        local input = vim.fn.input("Search in Files: ")
+
+        if input ~= "" then
+            telescope.grep_string({ search = input })
+        end
+    end, { desc = "Grep in Files" })
+end
 
 -- File Operations
 map("n", "<C-s>", "<cmd>w<CR>", { desc = "Save File" })
--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -29,7 +29,14 @@ return {
     {
         "MarcWeber/vim-addon-local-vimrc",
         config = function()
-
         end
     },
+    {
+        'nvim-telescope/telescope.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            -- optional but recommended
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        }
+    }
 }
